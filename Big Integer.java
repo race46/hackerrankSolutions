ef632ff4516d0e09ce10aa433d2d7784d61fec42
race46 <52047068+race46@users.noreply.github.com>
2021-01-31 03:29:31 +0300

    Create Big Integer.java

---
+++ Big Integer.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       
+        Scanner scanner=new Scanner(System.in);
+        BigInteger bigInteger=new BigInteger(scanner.next());
+        BigInteger bigInteger1=new BigInteger(scanner.next());
+        System.out.println(bigInteger1.add(bigInteger));
+        System.out.println(bigInteger1.multiply(bigInteger));
+    
+    }
+}
